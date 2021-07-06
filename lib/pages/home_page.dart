ca6538315ab22223793038730ef3179451613fe5
shahob22 <shahobtursunovdev22@gmail.com>
2021-07-06 19:41:39 +0500

    Yangilanish!

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -42,6 +42,7 @@ class _HomePageState extends State<HomePage> {
             ),
             Expanded(
                 child: Container(
+
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(topLeft: Radius.circular(50), topRight: Radius.circular(50))
